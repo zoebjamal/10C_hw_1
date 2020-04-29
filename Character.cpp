cb8db699e2cb5a6c4d7a39f16c419dbeff572bb7
zoebjamal <62960617+zoebjamal@users.noreply.github.com>
2020-04-30 02:16:03 +0530

    Update Character.cpp
    added semicolon, going to drastically change a line here, and then change the same line in experimentation branch to attempt to create merge conflict.

--- Character.cpp
+++ Character.cpp
@@ -15,13 +15,13 @@ Character::Character(string newName, int newHealth, int newDamage, int newArrows
 }
 
 void Character::set_health(int newHealth) {
-	health = newHealth
+	health = newHealth;
 }
 
 void Character::attack(Character& target) {
 	target.health -= damage;
 	std::cout << name << "attacks" << target.name << " doing " << damage << " damage!" << std::endl;
-	std::cout << target.name << " Health: " << target.health << std::endl;
+	std::cout << target.name << "'s health is " << target.health << std::endl;
 }
 
 void Character::rangedAttack(Character& target) {
